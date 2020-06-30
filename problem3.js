72a8b851a9a994ac796033f527ceccf15217cc7b
Ewelina Podlewska <ewelina.podlewska@gmail.com>
2020-06-30 11:49:52 +0200

    problem3_v2

--- problem3.js
+++ problem3.js
@@ -1,12 +1,9 @@
 let startingNumber = 600851475143;
-let primeFactors = [];
 for (let naturalNumber = 2; ; naturalNumber++) {
   if (startingNumber % naturalNumber === 0) {
-    primeFactors.push(naturalNumber);
     startingNumber = startingNumber / naturalNumber;
     if (startingNumber === 1) {
-      console.log(primeFactors);
-      console.log(primeFactors[primeFactors.length - 1]);
+      console.log(naturalNumber)
       break;
     }
   }
