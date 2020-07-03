5183b981956f81cbf22420919e87f516aaa27e7d
Ewelina Podlewska <ewelina.podlewska@gmail.com>
2020-07-03 18:35:11 +0200

    partial resolution

---
+++ problem4.js
@@ -0,0 +1,23 @@
+let firstWord = "mom";
+function reverseString(str) {
+  let splitString = str.split("");
+  let reverseArray = splitString.reverse("");
+  let joinArray = reverseArray.join("");
+  return joinArray;
+}
+let result = reverseString(firstWord);
+
+console.log(firstWord === result);
+
+let number = 9009;
+let numbertoString = number.toString();
+let numberCollection = [];
+console.log(typeof number);
+console.log(typeof numbertoString);
+
+for (let naturalNumber = 10; naturalNumber <= 99; naturalNumber++) {
+  for (let nextNumber = naturalNumber; nextNumber <= 99; nextNumber++) {
+    let lookedNumber = naturalNumber * nextNumber;
+  }
+}
+
