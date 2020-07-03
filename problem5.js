8a14dcb7a6a8ac45ce55e1d6ef082938aa87dd5c
Ewelina Podlewska <ewelina.podlewska@gmail.com>
2020-07-03 19:50:51 +0200

    example solution

---
+++ problem5.js
@@ -0,0 +1,19 @@
+
+for (let naturalNumber = 1; ; naturalNumber++) {
+  if (
+    naturalNumber % 1 === 0 &&
+    naturalNumber % 2 === 0 &&
+    naturalNumber % 3 === 0 &&
+    naturalNumber % 4 === 0 &&
+    naturalNumber % 5 === 0 &&
+    naturalNumber % 6 === 0 &&
+    naturalNumber % 7 === 0 &&
+    naturalNumber % 8 === 0 &&
+    naturalNumber % 9 === 0 &&
+    naturalNumber % 10 === 0
+  ) {
+    console.log(naturalNumber);
+    break;
+  }
+
+}
