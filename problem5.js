c5cac5bc7d2ba9fe7d0970ebddfd7d564e3306ca
Ewelina Podlewska <ewelina.podlewska@gmail.com>
2020-07-03 20:00:56 +0200

    problem5_solution

--- problem5.js
+++ problem5.js
@@ -10,7 +10,17 @@ for (let naturalNumber = 1; ; naturalNumber++) {
     naturalNumber % 7 === 0 &&
     naturalNumber % 8 === 0 &&
     naturalNumber % 9 === 0 &&
-    naturalNumber % 10 === 0
+    naturalNumber % 10 === 0 &&
+    naturalNumber % 11 === 0 &&
+    naturalNumber % 12 === 0 &&
+    naturalNumber % 13 === 0 &&
+    naturalNumber % 14 === 0 &&
+    naturalNumber % 15 === 0 &&
+    naturalNumber % 16 === 0 &&
+    naturalNumber % 17 === 0 &&
+    naturalNumber % 18 === 0 &&
+    naturalNumber % 19 === 0 &&
+    naturalNumber % 20 === 0
   ) {
     console.log(naturalNumber);
     break;
