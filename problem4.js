263c9fcde9ee3260fdc5674275ccc52837951c02
Ewelina Podlewska <ewelina.podlewska@gmail.com>
2020-07-03 18:53:51 +0200

    solution for example data

--- problem4.js
+++ problem4.js
@@ -1,23 +1,36 @@
-let firstWord = "mom";
-function reverseString(str) {
-  let splitString = str.split("");
-  let reverseArray = splitString.reverse("");
-  let joinArray = reverseArray.join("");
-  return joinArray;
-}
-let result = reverseString(firstWord);
+// let firstWord = "mom";
+// function reverseString(str) {
+//   let splitString = str.split("");
+//   let reverseArray = splitString.reverse("");
+//   let joinArray = reverseArray.join("");
+//   return joinArray;
+// }
+// let result = reverseString(firstWord);
 
-console.log(firstWord === result);
+// console.log(firstWord === result);
 
-let number = 9009;
-let numbertoString = number.toString();
-let numberCollection = [];
-console.log(typeof number);
-console.log(typeof numbertoString);
+// let number = 9009;
+// let numbertoString = number.toString();
+// console.log(typeof number);
+// console.log(typeof numbertoString);
 
 for (let naturalNumber = 10; naturalNumber <= 99; naturalNumber++) {
   for (let nextNumber = naturalNumber; nextNumber <= 99; nextNumber++) {
     let lookedNumber = naturalNumber * nextNumber;
+    let numberToBeChecked = lookedNumber.toString();
+
+    function reverseString(str) {
+      let splitString = str.split("");
+      let reverseArray = splitString.reverse("");
+      let joinArray = reverseArray.join("");
+      return joinArray;
+    }
+    let result = reverseString(numberToBeChecked);
+
+    if (numberToBeChecked === result) {
+      console.log(numberToBeChecked);
+    }
   }
 }
+
 
